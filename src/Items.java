409d8ae7c59072934eb9e68dd996bdb6e34e7479
Lachlan-RMIT-afk <S4256613@student.rmit.edu.au>
2026-05-13 09:05:33 +1000

    inv

--- src/Items.java
+++ src/Items.java
@@ -1,2 +1,33 @@
 public class Items {
+
+    //Creating instance variables for Items
+    private String itemName;
+    private int itemDamage;
+    private String itemDescription;
+    private int healingAmount;
+
+
+    //Using a Constructor so these parameters must be passed though and set to the instance variables above.
+    public Items(String itemName, int itemDamage, String itemDescription, int healingAmount) {
+        this.itemName = itemName;
+        this.itemDamage = itemDamage;
+        this.itemDescription = itemDescription;
+        this.healingAmount = healingAmount;
+
+    }
+
+    //Using getVariableName to return specific values of an Item when called.
+    public String getItemName() {
+        return itemName;
+    }
+    public int getItemDamage() {
+        return itemDamage;
+    }
+    public String getItemDescription() {
+        return itemDescription;
+    }
+    public int getHealingAmount() {
+        return healingAmount;
+    }
+
 }
