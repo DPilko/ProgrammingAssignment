8b90fe389372ff9aa72559873f98e067cdea1af6
Lachlan-RMIT-afk <S4256613@student.rmit.edu.au>
2026-05-15 09:12:54 +1000

    Created other items

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,11 @@ public static void main(){
     String playerName = userInput.nextLine();
     Player player = new Player(playerName);
 
-    //created first item
+    //created items
     Items brokenSword = new Items("BrokenSword", 5, "Flimsy Broken Sword", 0);
+    Items goldenSword = new Items("GoldenSword", 20, "Strong Golden Sword", 0);
+    Items healingPotion = new Items("HealingPotion", 0, "Healing Potion that will restore 50 health", 50);
+    Items key = new Items("Key", 1, "Key", 0);
 
     //Needed to create a player before I could add an item
     player.getInventory().addItem(brokenSword);
