5de031ed1a17c93891c2857198ce89c0a58e31e8
Ali Reza <s4244228@student.rmit.edu.au>
2026-05-30 18:07:05 +1000

    ---

--- src/Combat.java
+++ src/Combat.java
@@ -29,35 +29,35 @@ public class Combat {
                 playerAttack = player.attackDamage();
                 enemy.takeDamage(playerAttack);
 
-                System.out.printf("You have attacked %s and dealt %d damage.\n%s has %d health remaining\n\n",enemy.getName(),playerAttack,enemy.getName(),enemy.getHealth());
+                System.out.printf("You have attacked %s and dealt %d damage.\n%s has %d health remaining\n\n",
+                        enemy.getName(), playerAttack, enemy.getName(), enemy.getHealth());
 
                 enemyAttack = enemy.attack();
                 player.takeDamage(enemyAttack);
 
-                System.out.printf("%s has attacked you and dealt %d to you.\n",enemy.getName(),enemyAttack);
-
+                System.out.printf("%s has attacked you and dealt %d to you.\n", enemy.getName(), enemyAttack);
 
             }
 
             else if (combatDecision.equals("r")) {
                 playerRun = random.nextBoolean();
-                if(playerRun){
+                if (playerRun) {
                     System.out.println("You have fled combat");
                     break;
-                }
-                else{
+                } else {
                     System.out.println("You failed to run away and the " + enemy.getName() + " got a critical hit in!");
                     //Added the +1, s that player is given at least on more chance to fight
                     player.setHealth((player.getHealth() / 2) + 1);
                 }
             }
 
-            else{
+            else {
                 System.out.println("Invalid combat choice. Please try again.");
             }
         }
-        if(player.getHealth() <= 0){
+        if (player.getHealth() <= 0) {
             System.out.println("You have died in combat to " + enemy.getName() + "!");
         }
+        scanner.close();
     }
 }
